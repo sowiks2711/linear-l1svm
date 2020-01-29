58b7160fe4240bce40c19590b6930645d0350fde
sowiks <sowiks@student.mini.pw.edu.pl>
2020-01-30 00:46:19 +0100

    Add measurements

--- tests/test_sandbox.R
+++ tests/test_sandbox.R
@@ -4,64 +4,80 @@ library(dplyr)
 library(SparseM)
 library(microbenchmark)
 library(kolejkeR)
+library(parallel)
 
 data("warsaw_queues")
 data <- warsaw_queues %>% filter(!is.na(status)) %>% select(czasObslugi, liczbaCzynnychStan, liczbaKlwKolejce, status)
 
 data
-x <- data[,1:3]
-y <- data[,4]
-
+features <- data[,1:3]
+target <- data[,4]
+length(y)
 # l1svm, l2svm, implementation
 
-benchmark_svm <- function(size, repeats) {
-  train=sample(1:dim(data)[1],size)
+benchmark_svm <- function(x, y, size, repeats) {
+  train=sample(1:length(y),size)
   xTrain=x[train,]
   yTrain=y[train]
   s=scale(xTrain,center=TRUE,scale=TRUE)
+
 
   l2svm_their <- function() LiblineaR(data=s,target=yTrain,type=1,cost=0.1, epsilon = 0.001, bias=1, cross=0,verbose=FALSE)
   l1svm_their <- function() LiblineaR(data=s,target=yTrain,type=3,cost=0.1, epsilon = 0.001, bias=1, cross=0,verbose=FALSE)
   l1svm_ours <- function() l1svm(data=s,target=yTrain,cost=0.1, epsilon=0.001, bias=1)
 
-  microbenchmark(l2svm_their(), l1svm_their(), l1svm_ours(), times = repeats)
+  cbind(microbenchmark(l2svm_their(), l1svm_their(), l1svm_ours(), times = repeats), size=size)
 
 }
 
-measurements2 <- lapply(seq(1000,10000,1000), benchmark_svm(repeats = 5))
+measurementsParallel <- mclapply(seq(1000,10000,1000), function(s) benchmark_svm(x = features, y = targets, size = s, repeats = 5), mc.cores = getOption("mc.cores", 4L))
 
-measurements
+
+library(ggplot2)
+measurements2 %>% Reduce(rbind, .) %>% ggplot(aes(x = size, y = time, colour=expr)) +
+  geom_point()
+
+measurementsParallel %>% Reduce(rbind, .) %>% ggplot(aes(x=size, y = time, color=expr)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw()
+
+
+
+measurements[[100]]
 
 data.frame(seq(1000,10000,1000), measurements)
 
+library(AppliedPredictiveModeling)
+data(abalone)
+dim(abalone)
+head(abalone)
+abalone %>% group_by(Type) %>% count()
+wideData <- abalone %>% mutate(Type=ifelse(Type == "F", "F", "NotF"))
+wide_features <- wideData[,-1]
+wide_target <- wideData[,1]
+
+measurements_wide <- mclapply(
+  seq(1000,4000,250),
+  function(s) lapply(
+    seq(2,8,1),
+    function(sub) cbind(features=sub, benchmark_svm(x = wide_features[,1:sub], y = wide_target, size = s, repeats = 3))
+  ),
+  mc.cores = getOption("mc.cores", 4L)
+)
+measurements_wide %>% Reduce(rbind, .) %>% Reduce(rbind, .) %>%
+  ggplot(aes(x = size, y = time, color = factor(features))) +
+  geom_point() +
+  scale_color_brewer(type="qual") +
+  facet_wrap(~expr)
+
+measurements_wide %>% Reduce(rbind, .) %>% Reduce(rbind, .) %>% write.csv(., "measurements_for_many_features.csv", col.names = TRUE)
 
 
-benchmark_svm(5000, 1)
-benchmark_data <- Reduce(function(x, y) rbind, measurements)
 
-library(ggplot2)
-benchmark_data %>%
-  ggplot(aes(x = benchmark_data$time))
 
-res %>% mutate(size = 100)
-cbind(size=100, res)
-cat("Results for C=",co," : ",acc," accuracy.\n",sep="")
 
-# Sparsifying the iris dataset:
-iS=apply(iris[,1:4],2,function(a){a[a<quantile(a,probs=c(0.25))]=0;return(a)})
-irisSparse<-as.matrix.csr(iS)
 
-# Applying a similar methodology as above:
-xTrain=irisSparse[train,]
-xTest=irisSparse[-train,]
 
-# Re-train best model with best cost value.
-LiblineaR(data=xTrain,target=yTrain,type=3,cost=0.1,bias=1,verbose=FALSE)
 
-l1svm(data=xTrain,target=yTrain,cost=0.1, epsilon=0.001, bias=1)
-# Make prediction
-p=predict(m,xTest,proba=pr,decisionValues=TRUE)
 
-# Display confusion matrix
-res=table(p$predictions,yTest)
-print(res)
